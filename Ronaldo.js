fe516fe5ae4aa747458ffe767a37c616f243684d
NotDrakyn <leonardo.arsa1@gmail.com>
2024-10-01 16:16:45 -0300

    Add files via upload

---
+++ Ronaldo.js
@@ -0,0 +1,9 @@
+let num1 = Number("Digite o primeiro numero");
+let num2 = Number("Digite o segundo numero");
+
+function Subtrair (num1, num2){
+    let resultado1;
+    resultado1= num1 - num2;
+
+}
+console.log("O resultado é" +resultado1)
